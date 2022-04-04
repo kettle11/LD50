36bde026c4fa0649dc15ac543acffc90dc293edf
Ian Kettlewell <ian.kettlewell@gmail.com>
2022-04-03 21:06:58 -0400

    Fix web build

--- src/worm.rs
+++ src/worm.rs
@@ -1,97 +1,175 @@
 use crate::*;
 
 #[derive(Component, Clone)]
-struct Worm;
+pub struct Worm {
+    pub lerp_target: Option<f32>,
+    pub rockets_hit: usize,
+}
 
-pub fn run_worm(world: &mut World) {
-    let worm_count = world.query::<&Worm>().unwrap().iter().count();
-    //println!("WORM COUNT: {:?}", worm_count);
-    if worm_count == 0 {
-        let mut worm_body = Handle::default();
-        let mut worm_teeth = Handle::default();
-        let mut worm_inner_teeth = Handle::default();
-        (|meshes: &mut Assets<Mesh>, graphics: &mut Graphics| {
-            let mut mesh_data = MeshData::new();
+pub fn setup_worm(world: &mut World) {
+    let mut worm_body = Handle::default();
+    let mut worm_teeth = Handle::default();
+    let mut worm_inner_teeth = Handle::default();
+    let mut worm_sound = Handle::default();
 
-            create_worm_cylinder(
-                &mut mesh_data,
-                -Vec3::Y * 600.0,
-                Vec3::ZERO,
-                30,
-                250.0,
-                true,
-            );
-            worm_body = meshes.add(Mesh::new(graphics, mesh_data));
+    (|meshes: &mut Assets<Mesh>, sounds: &mut Assets<Sound>, graphics: &mut Graphics| {
+        worm_sound = sounds.load("assets/worm.wav");
 
-            let mut mesh_data = MeshData::new();
+        let mut mesh_data = MeshData::new();
 
-            let mut radius = 330.0;
-            let mut y_offset = Vec3::ZERO;
-            let mut resolution = 30;
-            let mut twist = 0.25;
-            for _ in 0..1 {
-                create_worm_teeth(&mut mesh_data, Vec3::Y, y_offset, resolution, radius, twist);
-                // radius *= 0.5;
-                y_offset -= Vec3::Y * 150.0;
-                resolution -= 5;
-                twist += 0.05
-            }
+        create_worm_cylinder(
+            &mut mesh_data,
+            -Vec3::Y * 3000.0,
+            Vec3::ZERO,
+            30,
+            250.0,
+            true,
+        );
+        create_worm_cylinder(
+            &mut mesh_data,
+            -Vec3::Y * 3000.0,
+            Vec3::ZERO,
+            30,
+            250.0,
+            false,
+        );
+        worm_body = meshes.add(Mesh::new(graphics, mesh_data));
 
-            worm_teeth = meshes.add(Mesh::new(graphics, mesh_data));
-            let mut mesh_data = MeshData::new();
+        let mut mesh_data = MeshData::new();
 
+        let mut radius = 330.0;
+        let mut y_offset = Vec3::ZERO;
+        let mut resolution = 30;
+        let mut twist = 0.25;
+        for _ in 0..1 {
             create_worm_teeth(&mut mesh_data, Vec3::Y, y_offset, resolution, radius, twist);
             // radius *= 0.5;
             y_offset -= Vec3::Y * 150.0;
             resolution -= 5;
-            twist += 0.05;
+            twist += 0.05
+        }
 
-            worm_inner_teeth = meshes.add(Mesh::new(graphics, mesh_data));
-        })
-        .run(world);
+        worm_teeth = meshes.add(Mesh::new(graphics, mesh_data));
+        let mut mesh_data = MeshData::new();
 
-        println!("SPAWNING");
-        let body = world.spawn((
-            worm_body,
-            Transform::new().with_position(Vec3::Y * -100.0),
-            Material::DEFAULT,
+        create_worm_teeth(
+            &mut mesh_data,
+            Vec3::Y,
+            y_offset,
+            resolution,
+            radius,
+            twist * 1.4,
+        );
+        // radius *= 0.5;
+        y_offset -= Vec3::Y * 150.0;
+        resolution -= 5;
+        twist += 0.05;
+
+        worm_inner_teeth = meshes.add(Mesh::new(graphics, mesh_data));
+    })
+    .run(world);
+
+    println!("SPAWNING");
+    let body = world.spawn((
+        worm_body,
+        Transform::new().with_position(Vec3::Y * -100.0),
+        Material::DEFAULT,
+        Color::from_srgb_hex(0x4B0082, 1.0),
+        Worm {
+            lerp_target: None,
+            rockets_hit: 0,
+        },
+    ));
+
+    let teeth = world.spawn((
+        worm_teeth,
+        Transform::new(),
+        Material::DEFAULT,
+        Color::from_srgb_hex(0x4B0082, 1.0),
+    ));
+
+    let mut audio_source = AudioSource::new().with_volume(20.0);
+    audio_source.play(&worm_sound, true);
+
+    let inner_teeth = world.spawn((
+        worm_inner_teeth,
+        Transform::new(),
+        Material::UNLIT,
+        Color::interpolate(
+            Color::RED.with_lightness(0.1).with_chroma(0.7),
             Color::from_srgb_hex(0x4B0082, 1.0),
-            Worm,
-        ));
+            0.5,
+        ),
+        audio_source,
+        //Collider::Sphere(400.0),
+    ));
 
-        let teeth = world.spawn((
-            worm_teeth,
-            Transform::new(),
-            Material::DEFAULT,
-            Color::from_srgb_hex(0x4B0082, 1.0),
-        ));
+    set_parent(world, Some(body), teeth);
+    set_parent(world, Some(body), inner_teeth);
+}
 
-        let inner_teeth = world.spawn((
-            worm_inner_teeth,
-            Transform::new(),
-            Material::UNLIT,
-            Color::interpolate(
-                Color::RED.with_lightness(0.1).with_chroma(0.7),
-                Color::from_srgb_hex(0x4B0082, 1.0),
-                0.5,
-            ),
-        ));
-
-        set_parent(world, Some(body), teeth);
-        set_parent(world, Some(body), inner_teeth);
-    }
-
+pub fn run_worm(world: &mut World) {
     // Move the worm
 
-    (|(transform, _worm): (&mut Transform, &Worm),
-      player: (&GlobalTransform, &CharacterController)| {
-        transform.position.y += 0.13;
+    (|(transform, worm): (&mut Transform, &mut Worm),
+      player: (
+        &GlobalTransform,
+        &CharacterController,
+        &mut RigidBody,
+        &mut AudioSource,
+    ),
+      game_state: &mut GameState,
+      audio_assets: &mut Assets<Sound>| {
+        match game_state.game_mode {
+            GameMode::Game => {
+                if let Some(lerp_target) = worm.lerp_target {
+                    let diff = lerp_target - transform.position.y;
+                    if diff < 1.0 {
+                        worm.lerp_target = None;
+                    } else {
+                        transform.position.y += diff * 0.02;
+                    }
+                } else {
+                    if transform.position.y > 3000. {
+                        transform.position.y += 0.05;
+                    } else {
+                        transform.position.y += 0.13;
+                    }
+                }
+                let worm_angry = worm.rockets_hit > 0;
 
-        let player_diff = player.0.position.y - (transform.position.y - 120.0);
+                if worm_angry {
+                    transform.position.y += 0.13;
+                }
 
-        println!("PLAYER DIFF: {:?}", player_diff);
-        if player_diff < 0.0 {
-            println!("PLAYER LOSES");
+                if worm.rockets_hit > 10 {
+                    transform.position.y += 0.13;
+                }
+
+                if worm.rockets_hit > 20 {
+                    worm.lerp_target = Some(3800.);
+                }
+
+                let player_diff = player.0.position.y - (transform.position.y - 120.0);
+
+                // transform.position.y = player.0.position.y - 100.;
+                //println!("PLAYER DIFF: {:?}", player_diff);
+                if player_diff < 0.0 {
+                    if worm_angry {
+                        player.2.velocity = Vec3::Y * 10_000.0 + Vec3::X * 2000.0;
+                        player.2.mutated_velocity = true;
+                        game_state.victory = true;
+                        player
+                            .3
+                            .play(&audio_assets.load("assets/upbeat_vibes.wav"), true);
+                        println!("PLAYER WINS?");
+                    } else {
+                        game_state.game_mode = GameMode::GameOver;
+                        println!("PLAYER LOSES");
+                    }
+                }
+            }
+            _ => {}
         }
     })
     .run(world);
